89dc066c01cf80fc64cba5a2b66f1d693d84a580
Kevin Novak <kevinmichaelnovak@gmail.com>
2021-07-24 19:40:21 -0400

    Add tsdoc example

--- src/my-class.ts
+++ src/my-class.ts
@@ -1,4 +1,9 @@
 export class MyClass {
+    /**
+     * Says hello to a person.
+     * @param name The name of the person to say hello to.
+     * @returns The phrase saying hello to the person.
+     */
     public sayHello(name: string): string {
         return `Hello ${name}!`;
     }
